3e08d038d752c34cff4aaeab10477b83d3819185
Md Moinuddin Kamal <cpimoinuddin@gmail.com>
2022-08-26 21:56:35 +0600

    first commit

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+ 
+    apiKey: "AIzaSyA_ezKy1FssPTLxZhTbZsbpW0lAUsg5Muw",
+    authDomain: "ecommerce-apps-3238b.firebaseapp.com",
+    projectId: "ecommerce-apps-3238b",
+    storageBucket: "ecommerce-apps-3238b.appspot.com",
+    messagingSenderId: "305233122134",
+    appId: "1:305233122134:web:5a30068e4cd6e44ed3bcf7",
+    measurementId: "G-MKNW16LZG5"
+  
+  };
+
+export default firebaseConfig;
